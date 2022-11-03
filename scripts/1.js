21292cb7b4a14da876a04d486d448f50c5cfd884
Leandro <leandrofisio94@gmail.com>
2022-11-03 16:21:56 -0300

    exercicio 7

---
+++ scripts/1.js
@@ -0,0 +1,6 @@
+function executaNumero (number) { 
+return number
+}
+const novoNumero = executaNumero(2)
+console.log(novoNumero)
+executaNumero(5)
---
+++ scripts/7.js
@@ -0,0 +1,12 @@
+function exterminadorDeZeros (numbers) {
+    const numeroSemZero = []
+    let counter = 0
+    while (counter <= numbers.length - 1){
+        if(numbers[counter] !== 0) {
+            numeroSemZero.push(numbers[counter])
+        }
+        counter++
+    }
+    return numeroSemZero
+}
+console.log(exterminadorDeZeros([1,6,3,8,3,0,7,0,4,3,2,0]))
