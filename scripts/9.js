159fadf313ab17f2de8ee677af30e17a30582c96
Leandro <leandrofisio94@gmail.com>
2022-11-07 10:43:33 -0300

    exercicio nove

---
+++ scripts/9.js
@@ -0,0 +1,7 @@
+function preservadorDeZeros (numbers) {
+    return numbers.filter(funcion(number))
+    return number = 0
+}
+const numerosParaSalvarZero = [1,4,6,0,3,4,0,5,6,0,1,1]
+const numerosSóZero = preservadorDeZeros(numerosParaSalvarZero)
+console.log(numerosSóZero)
